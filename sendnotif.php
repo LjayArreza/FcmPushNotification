fb3602224feb7f46883cdf29541d415881671aae
Ljay Arreza <35939426+LjayArreza@users.noreply.github.com>
2020-11-06 14:04:04 +0800

    Update sendnotif.php

--- sendnotif.php
+++ sendnotif.php
@@ -1,13 +1,13 @@
 <?php
 $ch=curl_init("https://fcm.googleapis.com/fcm/sendnotif");
-$header=array("Your content type here", "Authorization: Your Firebase Secret Key Here");
+$header=array("Your content type here", "Authorization: key=YOUR_SERVER_KEY");
 
 // send to all devices/topics
 $data=json_encode(array("to"=>"/topics/allDevices","notification"=>array("title"=>$_REQUEST['title'], "message"=>$_REQUEST['message'])));
 
 
 // for specific topic
-// $data=json_encode(array("to"=>"Your Device Token","notification"=>array("title"=>$_REQUEST['title'], "message"=>$_REQUEST['message'])));
+// $data=json_encode(array("to"=>"YOUR_DEVICE_TOKEN","notification"=>array("title"=>$_REQUEST['title'], "message"=>$_REQUEST['message'])));
 
 
 curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
